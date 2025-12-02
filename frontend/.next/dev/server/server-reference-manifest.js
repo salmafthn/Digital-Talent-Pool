fd63131c8d0ca309a51bcc10be7f3eb36d1b4dd7
Salma Adzra Fathina <125282444+salmafthn@users.noreply.github.com>
2025-12-02 19:59:58 +0700

    Delete frontend/.next/dev directory

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+oizQFKfSONwphO8FnfPT+FBbsVC1Zq2psFT8KxGnnc=\"\n}"
--- frontend/.next/dev/static/chunks/app_chatbot_page_tsx_a2c2d90a._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_8e3ec690._.js",
-  "static/chunks/node_modules__pnpm_36a6c908._.js"
-],
-    source: "dynamic"
-});
